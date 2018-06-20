3be28ed995f894029aaf5650dfcae6ea7d30326b
Stéphane <stephluethi@hotmail.com>
2018-06-20 11:46:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |combination_array| 
     combination_array.each do |combination| 
-      if board(combination) == "X" || "Y"
+      if board[combination] == "X" || "Y"
         combination_array
       end
     end
